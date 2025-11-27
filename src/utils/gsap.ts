4fb27f7949c3e203b6c8afd2b56de6248863d013
Antonio Wang <notreallyeight@gmail.com>
2025-11-27 15:59:59 +0100

    refactor: add JSDoc comments for slideUpFadeIn and opacityFadeIn functions

--- src/utils/gsap.ts
+++ src/utils/gsap.ts
@@ -1,9 +1,30 @@
 import Gsap from "gsap";
 
-export const slideUpFadeIn = (target: string) =>
-  Gsap.from(target, {
+/**
+ * Animates an element so that it slides up while fading in
+ * @param target - The target HTML element (should be an ID in most cases)
+ */
+export const slideUpFadeIn = (target: string): void =>
+  void Gsap.from(target, {
     opacity: 0,
     y: window.innerHeight,
     duration: 1,
     ease: "power3.out",
   });
+
+/**
+ * Sets the opacity of an element to animate it linearly so that it creates an opacity
+ * fade in effect
+ * @param target The target HTML element (should be an ID in most cases)
+ */
+export const opacityFadeIn = (target: string) => {
+  Gsap.set(target, {
+    opacity: 0,
+  });
+
+  Gsap.to(target, {
+    opacity: 1,
+    duration: 0.2,
+    ease: "none",
+  });
+};
