04f83de501ae761b76dfe34a8c04cc2c319e0b10
Antonio Wang <notreallyeight@gmail.com>
2026-01-25 17:52:38 +0100

    refactor: simplify font imports and update layout to use fewer font variables

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,59 +1,15 @@
-import {
-  IBM_Plex_Mono,
-  Manrope,
-  Montserrat,
-  Playfair_Display,
-  Roboto_Condensed,
-  Roboto_Slab,
-  Space_Grotesk,
-} from "next/font/google";
+import { IBM_Plex_Mono, Space_Grotesk } from "next/font/google";
 
 export const ibmPlexMono = IBM_Plex_Mono({
   subsets: ["latin"],
   display: "swap",
-  weight: ["200", "300", "400"],
-  style: ["italic", "normal"],
+  weight: "400",
   variable: "--font-ibm-plex-mono",
-});
-
-export const manrope = Manrope({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["200", "300", "400"],
-  variable: "--font-manrope",
-});
-
-export const montserrat = Montserrat({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["200", "600", "800"],
-  variable: "--font-montserrat",
-});
-
-export const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-  variable: "--font-playfair-display",
-});
-
-export const robotoCondensed = Roboto_Condensed({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "400",
-  variable: "--font-roboto-condensed",
-});
-
-export const robotoSlab = Roboto_Slab({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "700",
-  variable: "--font-roboto-slab",
 });
 
 export const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   display: "swap",
-  weight: ["300", "400"],
+  weight: ["300", "400", "700"],
   variable: "--font-space-grotesk",
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,8 @@
 import "./globals.css";
-import {
-  ibmPlexMono,
-  manrope,
-  montserrat,
-  playfairDisplay,
-  robotoCondensed,
-  robotoSlab,
-  spaceGrotesk,
-} from "./fonts";
+import { ibmPlexMono, spaceGrotesk } from "./fonts";
 import { Metadata } from "next";
 
-const fonts = [
-  ibmPlexMono.variable,
-  manrope.variable,
-  montserrat.variable,
-  playfairDisplay.variable,
-  robotoCondensed.variable,
-  robotoSlab.variable,
-  spaceGrotesk.variable,
-].join(" ");
+const fonts = [ibmPlexMono.variable, spaceGrotesk.variable].join(" ");
 
 // Next.js automatically updates metadata using this export.
 // eslint-disable-next-line react-refresh/only-export-components
