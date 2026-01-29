a795be4d659333ed2dbe40c1359683eca86a388f
Antonio Wang <notreallyeight@gmail.com>
2026-01-29 15:36:47 +0100

    refactor: enhance Button component with additional props for icon and full-width support

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,37 @@
+import React, { Activity } from "react";
+
 type Props = {
+  className?: string;
+  fullWidth?: boolean;
+  icon?: React.ReactElement;
   text: string;
   onSubmit: () => void;
   primary?: boolean;
 };
 
-const Button = ({ text, onSubmit, primary = true }: Props) => (
+const Button = ({
+  className,
+  fullWidth,
+  icon,
+  text,
+  onSubmit,
+  primary = true,
+}: Props) => (
   <button
     className={`${
       primary
         ? "bg-white text-primary hover:bg-primary hover:text-white"
         : "bg-primary text-white hover:bg-white hover:text-primary"
-    } p-4 font-family-button w-fit h-fit
+    } p-4 font-family-button ${fullWidth ? "w-full" : "w-fit"} h-fit
                          border-2 border-white duration-(--transition-duration)
-                         cursor-pointer`}
+                         cursor-pointer flex flex-row items-center justify-center space-x-4 ${className ?? ""}`}
     type="submit"
     onSubmit={onSubmit}
   >
-    {text}
+    <Activity mode={icon !== undefined ? "visible" : "hidden"}>
+      <div>{icon}</div>
+    </Activity>
+    <div>{text}</div>
   </button>
 );
 
