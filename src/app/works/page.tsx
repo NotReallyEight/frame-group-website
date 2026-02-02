2a1cf553c94786de18c5dbf3e40838f922d2441a
Antonio Wang <notreallyeight@gmail.com>
2026-02-02 19:43:56 +0100

    refactor: update Works component to integrate Metadata and enhance mobile navigation

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,30 +1,31 @@
-import { PickerLinkBackground } from "@/components/MainScreenPicker/PickerLinkBackground";
-import Navbar from "@/components/Navbar";
-import type { Metadata } from "next";
+"use client";
 
-// Next.js automatically updates metadata using this export.
-// eslint-disable-next-line react-refresh/only-export-components
-export const metadata: Metadata = {
-  title: "I Nostri Servizi - Frame",
-  description:
-    "Produzioni creative, eventi, sviluppo web e noleggio attrezzatura professionale. Scopri tutti i nostri servizi e trova la soluzione giusta per il tuo progetto.",
-  keywords:
-    "produzioni video, produzioni creative, eventi, copertura eventi, sviluppo web, web development, siti web professionali, servizi digitali, noleggio attrezzatura, rental attrezzatura video, fotografia professionale, videomaking, soluzioni creative",
-};
+import { PickerLinkBackground } from "@/components/MainScreenPicker/PickerLinkBackground";
+import Metadata from "@/components/Metadata";
+import Navbar from "@/components/Navbar";
+import { Activity, useState } from "react";
 
 export default function Works() {
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+
   return (
     <>
+      <Metadata
+        title="I Nostri Servizi - Frame"
+        description="Produzioni creative, eventi, sviluppo web e noleggio attrezzatura professionale. Scopri tutti i nostri servizi e trova la soluzione giusta per il tuo progetto."
+        keywords="produzioni video, produzioni creative, eventi, copertura eventi, sviluppo web, web development, siti web professionali, servizi digitali, noleggio attrezzatura, rental attrezzatura video, fotografia professionale, videomaking, soluzioni creative"
+      />
       <div className="grid grid-cols-1 md:grid-cols-[2rem_1fr] grid-rows-[auto_1fr] text-white h-dvh p-0">
         <div className="hidden md:flex ml-8 w-px bg-border h-full" />
         {/* Header */}
-        <Navbar />
-        <div className="hidden md:flex ml-8 w-px bg-border h-full" />
-        {/* Grid */}
-        <div>
-          <div className="absolute w-full h-px left-0 bg-border" />
-          <div
-            className="grid grid-cols-2 justify-center items-center h-full
+        <Navbar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+        <Activity mode={isNavOpen ? "hidden" : "visible"}>
+          <div className="hidden md:flex ml-8 w-px bg-border h-full" />
+          {/* Grid */}
+          <div>
+            <div className="absolute w-full h-px left-0 bg-border" />
+            <div
+              className="grid grid-cols-2 justify-center items-center h-full
 											 font-family-grid-label *:flex *:justify-start *:items-end
 											 [&_span]:-translate-x-1 lg:[&_span]:mb-8 lg:[&_span]:ml-8
 											 [&_span]:mb-4 [&_span]:ml-4
@@ -37,38 +38,39 @@ export default function Works() {
 											 [&_a_img]:duration-(--grid-fade-in-duration)
 											 [&_a_img]:opacity-0 [&_a_img]:absolute [&_a_img]:top-0
 											 [&_a_img]:left-0 [&_a_img]:-z-10"
-          >
-            <a href="/events" className="border-r border-r-border">
-              <span>EVENTI</span>
-              <PickerLinkBackground
-                alt="Foto di un evento"
-                src="/assets/main-screen-picker/events-demo.webp"
-              />
-            </a>
-            <a href="/productions">
-              <span>PRODUZIONI</span>
-              <PickerLinkBackground
-                alt="Foto di un ciak"
-                src="/assets/main-screen-picker/film-clapper-demo.webp"
-              />
-            </a>
-            <div className="absolute w-full h-px left-0 bg-border" />
-            <a href="/web-dev" className="border-r border-r-border">
-              <span>WEB DEV</span>
-              <PickerLinkBackground
-                alt="Foto di un computer con del codice"
-                src="/assets/main-screen-picker/web-dev-demo.webp"
-              />
-            </a>
-            <a href="/rental">
-              <span>RENTAL</span>
-              <PickerLinkBackground
-                alt="Foto di obiettivi"
-                src="/assets/main-screen-picker/rental-demo.webp"
-              />
-            </a>
+            >
+              <a href="/events" className="border-r border-r-border">
+                <span>EVENTI</span>
+                <PickerLinkBackground
+                  alt="Foto di un evento"
+                  src="/assets/main-screen-picker/events-demo.webp"
+                />
+              </a>
+              <a href="/productions">
+                <span>PRODUZIONI</span>
+                <PickerLinkBackground
+                  alt="Foto di un ciak"
+                  src="/assets/main-screen-picker/film-clapper-demo.webp"
+                />
+              </a>
+              <div className="absolute w-full h-px left-0 bg-border" />
+              <a href="/web-dev" className="border-r border-r-border">
+                <span>WEB DEV</span>
+                <PickerLinkBackground
+                  alt="Foto di un computer con del codice"
+                  src="/assets/main-screen-picker/web-dev-demo.webp"
+                />
+              </a>
+              <a href="/rental">
+                <span>RENTAL</span>
+                <PickerLinkBackground
+                  alt="Foto di obiettivi"
+                  src="/assets/main-screen-picker/rental-demo.webp"
+                />
+              </a>
+            </div>
           </div>
-        </div>
+        </Activity>
       </div>
     </>
   );
